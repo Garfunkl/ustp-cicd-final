d4aebb0bc5c652be5d0769d9f248e870d9d0d456
Simon Brandstetter <107547993+Garfunkl@users.noreply.github.com>
2026-01-26 21:29:53 +0100

    Change coverage reporter to include 'json-summary'

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     setupFiles: ['./src/test/setup.ts'],
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'lcov', 'html', 'json'],
+      reporter: ['text', 'lcov', 'html', 'json-summary'],
       exclude: [
         'node_modules/',
         'src/test/',
